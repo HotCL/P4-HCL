5b44341042550ba50150d57e08dee2c2b555cfa9
Abildgaard96 <nabild16@student.aau.dk>
2018-04-03 12:55:32 +0200

    Hclang 59 - Logger (#39)
    * HCLANG-59 wrote logger for multiple errorTypes
    
    * Created functionality for helptexts
    
    also added small indentation
    
    * minor changes and comments
    
    * HCLANG-59 Refactored to use classes instead of enums
    
    * HCLANG-59 refactorings for pretty code
    
    * rebase misc-files
    
    * HCLANG-59 tests and comments for the logger
    
    * added newLine to last print
    
    * small refactorings to cleans smells
    
    * Jira id: HCLANG-59 - #comment rebase develop
    
    * Jira id: HCLANG-59 - #comment fix conflicts

---
+++ code/src/main/kotlin/logger/MissingArgumentError.kt
@@ -0,0 +1,14 @@
+package logger
+
+import exceptions.ParserException
+
+/**
+ * Class used to log missing arguments.
+ * @param functionName The name of the function which needs arguemts
+ */
+class MissingArgumentError(lineNumber: Int, lineIndex: Int, lineText: String, private val functionName: String)
+    : ParserException(lineNumber, lineIndex, lineText){
+    override val errorType = "MISSING-ARGUMENT-ERROR"
+    override val errorMessage  = "Missing argument for function '$functionName'."
+    override val helpText = "Have you included all arguments for function?"
+}
