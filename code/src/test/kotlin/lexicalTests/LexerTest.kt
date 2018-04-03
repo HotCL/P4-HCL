efd27f5e8d481e0f0a5a5a90a12f13d93a666bc4
Casper Weiss Bang <master@thecdk.net>
2018-04-03 19:49:06 +0200

    Fixed errors regarding lexer and refactored a bit (#45)
    * Jira id: HCLANG-65 - #comment wip
    
    * Jira id: HCLANG-65 - #comment  renames
    
    * Jira id: HCLANG-65 - #comment now it atleast works. prettify 2cum
    
    * Jira id: HCLANG-65 - #comment pretty
    
    * Jira id: HCLANG-65 - #comment added possibility of comments
    
    * Jira id: HCLANG-65 - #comment  love me senpai
    
    * Jira id: HCLANG-65 - #comment figured out multiline no work. needs to be fixed
    
    * Jira id: HCLANG-65 - #comment used correct error type
    
    * Fixed multi word string literal
    
    * Fixed minor colon arrow issue

--- code/src/test/kotlin/lexicalTests/LexerTest.kt
+++ code/src/test/kotlin/lexicalTests/LexerTest.kt
@@ -4,14 +4,14 @@ import lexer.Lexer
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import lexer.PositionalToken
-import org.intellij.lang.annotations.Identifier
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import logger.StringDoesntEndError
+import org.junit.jupiter.api.Assertions.*
+import java.lang.reflect.Type
 
 class LexerTest {
     @org.junit.jupiter.api.Test
     fun lexerTestTokenGeneration() {
-        val lex = Lexer("var x = 5 + 7\nx = x times 10;")
+        val lex = Lexer("var x = 5 + 7\nx = x times\\\n10.0;")
 
         val tokensPositional = lex.getTokenSequence().toList()
 
@@ -58,14 +58,61 @@ class LexerTest {
         assertPositionalToken(tokensPositional[10],
                 { token -> token is lexer.Token.Identifier && token.value == "times" },
                 6, 1)
-
         assertPositionalToken(tokensPositional[11],
-                { token -> token is lexer.Token.Literal.Number && token.value == 10.0 },
-                12, 1)
+                { token -> token is lexer.Token.SpecialChar.LineContinue },
+                11, 1)
         assertPositionalToken(tokensPositional[12],
                 { token -> token is lexer.Token.SpecialChar.EndOfLine },
-                14, 1)
+                12, 1)
 
+        assertPositionalToken(tokensPositional[13],
+                { token -> token is lexer.Token.Literal.Number && token.value == 10.0 },
+                0, 2)
+        assertPositionalToken(tokensPositional[14],
+                { token -> token is lexer.Token.SpecialChar.EndOfLine },
+                4, 2)
+
+    }
+
+    @org.junit.jupiter.api.Test
+    fun lexerTestIgnoreComments() {
+        val lex = Lexer("var x = 5# fem er et ret cool tal tbh\nx = 4")
+
+        val tokensPositional = lex.getTokenSequence().toList()
+
+        assertPositionalToken(tokensPositional[0],
+                { token -> token is lexer.Token.Type.Var },
+                0, 0)
+
+        assertPositionalToken(tokensPositional[1],
+                { token -> token is lexer.Token.Identifier && token.value == "x" },
+                4, 0)
+
+        assertPositionalToken(tokensPositional[2],
+                { token -> token is lexer.Token.SpecialChar.Equals },
+                6, 0)
+
+        assertPositionalToken(tokensPositional[3],
+                { token -> token is lexer.Token.Literal.Number && token.value == 5.0 },
+                8, 0)
+
+        assertPositionalToken(tokensPositional[4],
+                { token -> token is lexer.Token.SpecialChar.EndOfLine},
+                9, 0)
+
+        assertPositionalToken(tokensPositional[5],
+                { token -> token is lexer.Token.Identifier && token.value == "x" },
+                0, 1)
+
+        assertPositionalToken(tokensPositional[6],
+                { token -> token is lexer.Token.SpecialChar.Equals },
+                2, 1)
+
+        assertPositionalToken(tokensPositional[7],
+                { token -> token is lexer.Token.Literal.Number && token.value == 4.0 },
+                4, 1)
+
+        assertEquals(9, tokensPositional.count())
     }
 
     @org.junit.jupiter.api.Test
@@ -270,10 +317,39 @@ class LexerTest {
     private fun assertPositionalToken(positionalToken: PositionalToken, validationExpression: (lexer.Token) -> Boolean,
                                       expectedLineIndex:Int, ExpectedLineNumber:Int)
     {
-        assertTrue(positionalToken.token.let { token -> validationExpression(token) })
+        assertTrue(positionalToken.token.let { token -> validationExpression(token) }, "Failed on: ${positionalToken.token}")
         assertThat(positionalToken.lineIndex, equalTo(expectedLineIndex))
         assertThat(positionalToken.lineNumber, equalTo(ExpectedLineNumber))
     }
+
+    @org.junit.jupiter.api.Test
+    fun lexerUnfinishedStringFails() {
+        assertThrows(StringDoesntEndError::class.java, {Lexer("\"hej").getTokenSequence().toList()})
+        assertThrows(StringDoesntEndError::class.java, {Lexer("'hej").getTokenSequence().toList()})
+    }
+
+    @org.junit.jupiter.api.Test
+    fun multiWordString() {
+        val rawString = "hej med dig"
+        val tokensPositional = Lexer("\"$rawString\"").getTokenSequence().toList()
+
+        assertPositionalToken(tokensPositional[0],
+                { token -> token is lexer.Token.Literal.Text && token.value == rawString},
+                0, 0)
+        assertEquals(2, tokensPositional.count())
+    }
+
+    @org.junit.jupiter.api.Test
+    fun multiWordStringEscapedQuotes() {
+        val rawString = "hej \\\"med\\\" dig"
+        val tokensPositional = Lexer("\"$rawString\"").getTokenSequence().toList()
+
+        assertPositionalToken(tokensPositional[0],
+                { token -> token is lexer.Token.Literal.Text && token.value == rawString},
+                0, 0)
+        assertEquals(2, tokensPositional.count())
+    }
+
 
     @org.junit.jupiter.api.Test
     fun lexerTestGetLine() {
