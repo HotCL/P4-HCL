0e37a47184393164015ea87896590080cc84705c
Unknown <tobiaslambek@gmail.com>
2018-04-03 12:36:44 +0200

    HCLANG-63 More tests - not all succeed as of this time

--- code/src/test/kotlin/lexicalTests/LexerTest.kt
+++ code/src/test/kotlin/lexicalTests/LexerTest.kt
@@ -77,6 +77,143 @@ class LexerTest {
     }
 
     @org.junit.jupiter.api.Test
+    fun lexerTestTypeDeclarationTokens() {
+        val lex = Lexer("var none num func tuple list bool txt")
+
+        val tokens = lex.getTokenSequence().toList()
+
+        assertTrue(tokens[0].token is lexer.Token.Type.Var)
+        assertTrue(tokens[1].token is lexer.Token.Type.None)
+        assertTrue(tokens[2].token is lexer.Token.Type.Number)
+        assertTrue(tokens[3].token is lexer.Token.Type.Func)
+        assertTrue(tokens[4].token is lexer.Token.Type.Tuple)
+        assertTrue(tokens[5].token is lexer.Token.Type.List)
+        assertTrue(tokens[6].token is lexer.Token.Type.Bool)
+        assertTrue(tokens[7].token is lexer.Token.Type.Text)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun lexerTestLegalIdentifiers() {
+        val lex = Lexer("hej var1 +- æøå ↑ ☺☻ ??? _this £50 *bold* 6lol")
+
+        val tokens = lex.getTokenSequence().toList()
+
+        tokens.dropLast(1).forEach{ assertTrue(it.token is lexer.Token.Identifier) }
+        assertTrue(tokens.last().token is lexer.Token.SpecialChar.EndOfLine)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun lexerTestIllegalIdentifiers() {
+        val lex = Lexer("var = \"id \"id\" 'di 'di' [] { () true \\ , }")
+        //Tokens: var, =, "id, "id", 'di, 'di', [, ], {, (, ), true, \, ",", ->, }
+        val tokens = lex.getTokenSequence().toList()
+
+        tokens.dropLast(1).forEach{ assertTrue(it.token !is lexer.Token.Identifier) }
+        assertTrue(tokens.last().token is lexer.Token.SpecialChar.EndOfLine)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun lexerTestBooleanLiteralTokens() {
+        val lex = Lexer("true false")
+
+        val tokens = lex.getTokenSequence().toList()
+
+        assertTrue(tokens[0].token is lexer.Token.Literal.Bool)
+        assertTrue(tokens[1].token is lexer.Token.Literal.Bool)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun lexerTestFunctionDeclaration() {
+        val lex = Lexer("func print = (txt message) -> none\n{ }")
+
+        val tokens = lex.getTokenSequence().toList()
+
+        assertPositionalToken(tokens[0],
+                { token -> token is lexer.Token.Type.Func },
+                0, 0)
+        assertPositionalToken(tokens[1],
+                { token -> token is lexer.Token.Identifier && token.value == "print" },
+                5, 0)
+        assertPositionalToken(tokens[2],
+                { token -> token is lexer.Token.SpecialChar.Equals },
+                11, 0)
+        assertPositionalToken(tokens[3],
+                { token -> token is lexer.Token.SpecialChar.ParenthesesStart },
+                13, 0)
+        assertPositionalToken(tokens[4],
+                { token -> token is lexer.Token.Type.Text },
+                14, 0)
+        assertPositionalToken(tokens[5],
+                { token -> token is lexer.Token.Identifier && token.value == "message" },
+                18, 0)
+        assertPositionalToken(tokens[6],
+                { token -> token is lexer.Token.SpecialChar.ParenthesesEnd },
+                25, 0)
+        assertPositionalToken(tokens[7],
+                { token -> token is lexer.Token.SpecialChar.Arrow },
+                27, 0)
+        assertPositionalToken(tokens[8],
+                { token -> token is lexer.Token.Type.None },
+                30, 0)
+        assertPositionalToken(tokens[9],
+                { token -> token is lexer.Token.SpecialChar.EndOfLine },
+                34, 0)
+        assertPositionalToken(tokens[10],
+                { token -> token is lexer.Token.SpecialChar.BlockStart },
+                0, 1)
+        assertPositionalToken(tokens[11],
+                { token -> token is lexer.Token.SpecialChar.BlockEnd },
+                2, 1)
+        assertPositionalToken(tokens[12],
+                { token -> token is lexer.Token.SpecialChar.EndOfLine },
+                3, 1)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun lexerTestInlineLambdaExpression() {
+        val lex = Lexer("x = 1 to 10 map {value * 2}")
+
+        val tokens = lex.getTokenSequence().toList()
+
+        assertPositionalToken(tokens[0],
+                { token -> token is lexer.Token.Identifier && token.value == "x" },
+                0, 0)
+        assertPositionalToken(tokens[1],
+                { token -> token is lexer.Token.SpecialChar.Equals },
+                2, 0)
+        assertPositionalToken(tokens[2],
+                { token -> token is lexer.Token.Literal.Number && token.value == 1.0 },
+                4, 0)
+        assertPositionalToken(tokens[3],
+                { token -> token is lexer.Token.Identifier && token.value == "to" },
+                6, 0)
+        assertPositionalToken(tokens[4],
+                { token -> token is lexer.Token.Literal.Number && token.value == 10.0 },
+                9, 0)
+        assertPositionalToken(tokens[5],
+                { token -> token is lexer.Token.Identifier && token.value == "map" },
+                12, 0)
+        assertPositionalToken(tokens[6],
+                { token -> token is lexer.Token.SpecialChar.BlockStart },
+                16, 0)
+        assertPositionalToken(tokens[7],
+                { token -> token is lexer.Token.Identifier && token.value == "value" },
+                17, 0)
+        assertPositionalToken(tokens[8],
+                { token -> token is lexer.Token.Identifier && token.value == "*" },
+                23, 0)
+        assertPositionalToken(tokens[9],
+                { token -> token is lexer.Token.Literal.Number && token.value == 2.0 },
+                25, 0)
+        assertPositionalToken(tokens[10],
+                { token -> token is lexer.Token.SpecialChar.BlockEnd },
+                26, 0)
+        assertPositionalToken(tokens[11],
+                { token -> token is lexer.Token.SpecialChar.EndOfLine },
+                27, 0)
+    }
+
+    @org.junit.jupiter.api.Test
     fun lexerTestGetLine() {
         val lexer = Lexer("Hej\nMed\nDig!")
         assertThat(lexer.inputLine(0), equalTo("Hej\n"))
