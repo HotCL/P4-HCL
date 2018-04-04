bfad70ec7a507886779119215179d464c606bcb5
Thomas Knudsen <teknight@live.dk>
2018-04-04 11:51:46 +0200

    Jira id: HCLANG-70 - #comment Change error to show class name instead of predefined string

--- code/src/main/kotlin/exceptions/TypeError.kt
+++ code/src/main/kotlin/exceptions/TypeError.kt
@@ -10,7 +10,6 @@ class TypeError(lineNumber: Int, lineIndex: Int, lineText: String,
                 : ParserException(lineNumber, lineIndex, lineText) {
     private val types: String = findTypes()
 
-    override val errorType = "TYPE-ERROR"
     override val errorMessage = "Function '$operator' not defined for types: $types."
     override val helpText = "Try casting your types to match eachother."
 
