9ac979854d59284f0a174e2c3800efea9e130b7f
Thomas Knudsen <teknight@live.dk>
2018-04-08 14:33:35 +0200

    Added symbol table implementation with tests

--- code/src/test/kotlin/symbolTableTests/SymbolTableTest.kt
+++ code/src/test/kotlin/symbolTableTests/SymbolTableTest.kt
@@ -1,57 +1,85 @@
 package symbolTableTests
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import parser.TreeNode
-import parser.symbolTable.Symbol
+import parser.symbolTable.SymbolTable
 
 class SymbolTableTest {
-
     @Test
-    fun testSymbolClassError() {
-        val symbol = Symbol(listOf())
-        assertTrue(symbol.undeclared)
-        assertTrue(!symbol.isIdentifier)
-        assertTrue(!symbol.isFunctions)
-        symbol.handle(
-                { assertTrue(false) },
-                { assertTrue(false) },
-                { assertTrue(true)  }
-        )
+    fun testIdentifierSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("x", TreeNode.Type.Number())
+        st.enterSymbol("y", TreeNode.Type.Text())
+        assertTrue(st.retrieveSymbol("x").identifier is TreeNode.Type.Number)
+        assertTrue(st.retrieveSymbol("y").identifier is TreeNode.Type.Text)
     }
 
     @Test
-    fun testSymbolClassId() {
-        val symbol = Symbol(listOf(TreeNode.Type.Number()))
-        assertTrue(!symbol.undeclared)
-        assertTrue(symbol.isIdentifier)
-        assertTrue(!symbol.isFunctions)
-        symbol.handle(
-                { assertTrue(false) },
-                { id -> assertTrue(id is TreeNode.Type.Number) },
-                { assertTrue(false) }
-        )
+    fun testIdentifierMultipleScopesSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("x", TreeNode.Type.Number())
+        st.openScope()
+        st.enterSymbol("x", TreeNode.Type.Text())
+        assertTrue(st.retrieveSymbol("x").identifier is TreeNode.Type.Text)
+        st.closeScope()
+        assertTrue(st.retrieveSymbol("x").identifier is TreeNode.Type.Number)
     }
 
     @Test
-    fun testSymbolClassFunDeclarations() {
-        val symbol = Symbol(listOf(
-                TreeNode.Type.Func(listOf(TreeNode.Type.Number()), TreeNode.Type.Number()),
-                TreeNode.Type.Func(listOf(TreeNode.Type.Text()), TreeNode.Type.Text())
-        ))
-        assertTrue(!symbol.undeclared)
-        assertTrue(!symbol.isIdentifier)
-        assertTrue(symbol.isFunctions)
-        symbol.handle(
-                { funDeclarations ->
-                    assertTrue(funDeclarations[0].paramTypes[0] is TreeNode.Type.Number)
-                    assertTrue(funDeclarations[0].returnType is TreeNode.Type.Number)
-                    assertTrue(funDeclarations[1].paramTypes[0] is TreeNode.Type.Text)
-                    assertTrue(funDeclarations[1].returnType is TreeNode.Type.Text)
-                },
-                { assertTrue(false) },
-                { assertTrue(false) }
-        )
+    fun testIdentifierUndeclaredSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("x", TreeNode.Type.Number())
+        assertTrue(st.retrieveSymbol("y").undeclared)
     }
 
+    @Test
+    fun testFunctionDeclarationsSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("myFun", TreeNode.Type.Func(listOf(TreeNode.Type.Number()), TreeNode.Type.Bool()))
+        val symbol = st.retrieveSymbol("myFun").functions
+        assertTrue(symbol[0].paramTypes[0] is TreeNode.Type.Number)
+        assertTrue(symbol[0].returnType is TreeNode.Type.Bool)
+    }
+
+    @Test
+    fun testFunctionDeclarationsMultipleScopesSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("myFun", TreeNode.Type.Func(listOf(TreeNode.Type.Number()), TreeNode.Type.Bool()))
+        st.enterSymbol("myFun", TreeNode.Type.Func(listOf(TreeNode.Type.Bool()), TreeNode.Type.Number()))
+        st.openScope()
+        val symbol = st.retrieveSymbol("myFun").functions
+        assertTrue(symbol[0].paramTypes[0] is TreeNode.Type.Number)
+        assertTrue(symbol[0].returnType is TreeNode.Type.Bool)
+        assertTrue(symbol[1].paramTypes[0] is TreeNode.Type.Bool)
+        assertTrue(symbol[1].returnType is TreeNode.Type.Number)
+        st.closeScope()
+    }
+
+    @Test
+    fun testFunctionDeclarationsMultipleScopesWithIdentifierInSeparateScopeSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("myFun", TreeNode.Type.Number())
+        st.openScope()
+        st.enterSymbol("myFun", TreeNode.Type.Func(listOf(TreeNode.Type.Number()), TreeNode.Type.Bool()))
+        st.enterSymbol("myFun", TreeNode.Type.Func(listOf(TreeNode.Type.Bool()), TreeNode.Type.Number()))
+        st.openScope()
+        val symbol = st.retrieveSymbol("myFun").functions
+        assertTrue(symbol[0].paramTypes[0] is TreeNode.Type.Number)
+        assertTrue(symbol[0].returnType is TreeNode.Type.Bool)
+        assertTrue(symbol[1].paramTypes[0] is TreeNode.Type.Bool)
+        assertTrue(symbol[1].returnType is TreeNode.Type.Number)
+        st.closeScope()
+        st.closeScope()
+        assertTrue(st.retrieveSymbol("myFun").identifier is TreeNode.Type.Number)
+    }
+
+    @Test
+    fun testIdentifierAlreadyDeclaredSymbolTable() {
+        val st = SymbolTable()
+        st.enterSymbol("x", TreeNode.Type.Number())
+        assertFalse(st.enterSymbol("x", TreeNode.Type.Text()))
+        assertTrue(st.retrieveSymbol("x").identifier is TreeNode.Type.Number)
+    }
 }
