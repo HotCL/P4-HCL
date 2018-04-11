9220eabe81587b86d76a870cdf85d36115fcd179
jones94 <jh13@student.aau.dk>
2018-04-11 14:14:15 +0200

    Importet a few references to shorten the qualified names used in references. Reason -> to match the consistency in other files.

--- code/src/main/kotlin/generation/GraphvizPrinter.kt
+++ code/src/main/kotlin/generation/GraphvizPrinter.kt
@@ -2,6 +2,8 @@ package generation
 
 import parser.AbstractSyntaxTree
 import parser.TreeNode
+import parser.TreeNode.Command
+import parser.TreeNode.Type
 
 class GraphvizPrinter : IPrinter {
     override fun generateOutput(ast: AbstractSyntaxTree): String =
@@ -22,23 +24,23 @@ class GraphvizPrinter : IPrinter {
     private fun TreeNode.visit(parentId:Int): String {
         val id = getNextId()
         return "# this is: $id - ${this}\n"+when (this) {
-            is TreeNode.Command.Assignment -> toLabel(id,this.identifier.name +"=")+
+            is Command.Assignment -> toLabel(id,this.identifier.name +"=")+
                     this.expression.visit(id)
-            is TreeNode.Command.Declaration -> toLabel(id,"${this.identifier.name}=")+
+            is Command.Declaration -> toLabel(id,"${this.identifier.name}=")+
                     this.type.visit(id)+if(this.expression != null) this.expression.visit(id) else ""
-            is TreeNode.Command.Expression.LambdaExpression -> toLabel(id,"Lambda")+this.returnType.visit(id)+
+            is Command.Expression.LambdaExpression -> toLabel(id,"Lambda")+this.returnType.visit(id)+
                 this.paramDeclarations.visit(id)+this.body.visit(id)
 
-            is TreeNode.Command.Expression.Value.Literal.List -> toLabel(id,"Literal: List")+
+            is Command.Expression.Value.Literal.List -> toLabel(id,"Literal: List")+
                     this.elements.joinToString("\n") { it.visit(id) }
 
-            is TreeNode.Command.Expression.Value.Literal.Tuple -> toLabel(id,"Literal: Tuple")+
+            is Command.Expression.Value.Literal.Tuple -> toLabel(id,"Literal: Tuple")+
                     this.elements.joinToString("\n") { it.visit(id) }
 
-            is TreeNode.Command.Return -> toLabel(id,"return")+
+            is Command.Return -> toLabel(id,"return")+
                     this.expression.visit(id)
-            is TreeNode.Type -> this.visitType(id)
-            is TreeNode.Command.Expression.FunctionCall ->
+            is Type -> this.visitType(id)
+            is Command.Expression.FunctionCall ->
                 toLabel(id,"call: "+this.identifier.name) + this.parameters.joinToString("\n")
                 {it.visit(id) }
             is TreeNode.ParameterDeclaration -> toLabel(id,"parameter")+
@@ -47,22 +49,22 @@ class GraphvizPrinter : IPrinter {
         }+connectNodes(id,parentId)
     }
 
-    private fun TreeNode.Type.visitType(id: Int): String = toLabel(id,this.getTypeName()) + when(this){
-        is TreeNode.Type.List -> this.elementType.visit(id)
-        is TreeNode.Type.Func.ExplicitFunc -> this.paramTypes.joinToString("\n") {it.visit(id) }
-        is TreeNode.Type.Tuple -> this.elementTypes.joinToString("\n") {it.visit(id) }
+    private fun Type.visitType(id: Int): String = toLabel(id,this.getTypeName()) + when(this){
+        is Type.List -> this.elementType.visit(id)
+        is Type.Func.ExplicitFunc -> this.paramTypes.joinToString("\n") {it.visit(id) }
+        is Type.Tuple -> this.elementTypes.joinToString("\n") {it.visit(id) }
         else -> toLabel(id,this.getTypeName())
 
     }
 
-    private fun TreeNode.Type.getTypeName() : String = "TYPE:"+when(this){
-        TreeNode.Type.Number -> "num"
-        TreeNode.Type.Text -> "text"
-        TreeNode.Type.Bool -> "bool"
-        TreeNode.Type.None -> "none"
-        is TreeNode.Type.GenericType -> "T"
-        TreeNode.Type.Func.ImplicitFunc -> "func"
-        is TreeNode.Type.Func.ExplicitFunc -> "func"
+    private fun Type.getTypeName() : String = "TYPE:"+when(this){
+        Type.Number -> "num"
+        Type.Text -> "text"
+        Type.Bool -> "bool"
+        Type.None -> "none"
+        is Type.GenericType -> "T"
+        Type.Func.ImplicitFunc -> "func"
+        is Type.Func.ExplicitFunc -> "func"
         else -> this::class.simpleName.toString()
     }
 }
--- code/src/main/kotlin/generation/SourceCodePrinter.kt
+++ code/src/main/kotlin/generation/SourceCodePrinter.kt
@@ -1,54 +1,55 @@
 package generation
 
 import parser.AbstractSyntaxTree
-import parser.TreeNode
+import parser.TreeNode.Command
+import parser.TreeNode.Type
 
 class SourceCodePrinter : IPrinter {
     override fun generateOutput(ast: AbstractSyntaxTree): String = ast.children.visit()
 
-    private fun List<TreeNode.Command>.visit(): String = "${this.joinToString("\n") { it.visit() }}\n"
+    private fun List<Command>.visit(): String = "${this.joinToString("\n") { it.visit() }}\n"
 
-    private fun TreeNode.Command.visit(): String = when (this) {
-        is TreeNode.Command.Assignment -> this.visit()
-        is TreeNode.Command.Declaration -> this.visit()
-        is TreeNode.Command.Expression -> this.visit()
-        is TreeNode.Command.Return -> "return ${this.expression.visit()}"
+    private fun Command.visit(): String = when (this) {
+        is Command.Assignment -> this.visit()
+        is Command.Declaration -> this.visit()
+        is Command.Expression -> this.visit()
+        is Command.Return -> "return ${this.expression.visit()}"
     }
 
-    private fun TreeNode.Command.Assignment.visit(): String =
+    private fun Command.Assignment.visit(): String =
             "${this.identifier.name} = ${this.expression.visit()}"
 
-    private fun TreeNode.Command.Declaration.visit(): String =
+    private fun Command.Declaration.visit(): String =
             "${this.type.visit()} ${this.identifier.name}"+
                     if (this.expression != null)
                         " = ${this.expression.visit()}"
                     else ""
 
 
-    private fun TreeNode.Command.Expression.visit(): String = when(this){
-        is TreeNode.Command.Expression.Value.Identifier -> this.name
-        is TreeNode.Command.Expression.Value.Literal.Number -> this.value.toString()
-        is TreeNode.Command.Expression.Value.Literal.Text -> "\"${this.value}\""
-        is TreeNode.Command.Expression.Value.Literal.Bool -> this.value.toString().capitalize()
-        is TreeNode.Command.Expression.Value.Literal.Tuple -> this.visit()
-        is TreeNode.Command.Expression.Value.Literal.List -> this.visit()
-        is TreeNode.Command.Expression.LambdaExpression -> this.visit()
-        is TreeNode.Command.Expression.FunctionCall -> this.visit()
+    private fun Command.Expression.visit(): String = when(this){
+        is Command.Expression.Value.Identifier -> this.name
+        is Command.Expression.Value.Literal.Number -> this.value.toString()
+        is Command.Expression.Value.Literal.Text -> "\"${this.value}\""
+        is Command.Expression.Value.Literal.Bool -> this.value.toString().capitalize()
+        is Command.Expression.Value.Literal.Tuple -> this.visit()
+        is Command.Expression.Value.Literal.List -> this.visit()
+        is Command.Expression.LambdaExpression -> this.visit()
+        is Command.Expression.FunctionCall -> this.visit()
     }
 
-    private fun TreeNode.Command.Expression.Value.Literal.Tuple.visit(): String =
+    private fun Command.Expression.Value.Literal.Tuple.visit(): String =
             "(${this.elements.joinToString(",") { it.visit() }})"
 
 
-    private fun TreeNode.Command.Expression.Value.Literal.List.visit(): String =
+    private fun Command.Expression.Value.Literal.List.visit(): String =
             "[${this.elements.joinToString(",") { it.visit() }}]"
 
-    private fun TreeNode.Command.Expression.LambdaExpression.visit(): String =
+    private fun Command.Expression.LambdaExpression.visit(): String =
             "(${this.paramDeclarations.joinToString(", ") { "${it.type.visit()} ${it.identifier.name}" }}):"+
                     " ${this.returnType.visit()} {\n${this.body.visit()}}"
 
 
-    private fun TreeNode.Command.Expression.FunctionCall.visit(): String =
+    private fun Command.Expression.FunctionCall.visit(): String =
             if(parameters.count()==0) this.identifier.name
             else
             {
@@ -60,17 +61,17 @@ class SourceCodePrinter : IPrinter {
 
             }
 
-    private fun TreeNode.Type.visit(): String = when(this){
-        TreeNode.Type.Number -> "num"
-        TreeNode.Type.Text -> "text"
-        TreeNode.Type.Bool -> "bool"
-        TreeNode.Type.None -> "none"
-        is TreeNode.Type.GenericType -> "T"
-        is TreeNode.Type.List -> "list[${this.elementType.visit()}]"
-        is TreeNode.Type.Func.ExplicitFunc ->
+    private fun Type.visit(): String = when(this){
+        Type.Number -> "num"
+        Type.Text -> "text"
+        Type.Bool -> "bool"
+        Type.None -> "none"
+        is Type.GenericType -> "T"
+        is Type.List -> "list[${this.elementType.visit()}]"
+        is Type.Func.ExplicitFunc ->
             "func[${this.paramTypes.joinToString(", ") {it.visit() }}, ${this.returnType.visit()}]"
-        TreeNode.Type.Func.ImplicitFunc -> "func"
-        is TreeNode.Type.Tuple ->
+        Type.Func.ImplicitFunc -> "func"
+        is Type.Tuple ->
             "tuple[${this.elementTypes.joinToString(", ") {it.visit() }}]"
     }
 
