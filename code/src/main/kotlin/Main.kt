e376de2d9f3e6786f351cd682e94daa2f09adcb2
Thomas Knudsen <teknight@live.dk>
2018-04-12 14:11:49 +0200

    Jira id: HCLANG-76 - #comment Added stuff to main, to ease Casas life...

--- code/src/main/kotlin/Main.kt
+++ code/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
+import lexer.Lexer
+import parser.Parser
+
 fun main(args: Array<String>) {
-    println("Compiling stuff...")
+    val code = "" +
+            "list[num] myList = [5]\n" +
+            "list[list[num]] myListList = [myList]"
+
+    val lexer = Lexer(code)
+    println("Tokens: \n" + lexer.getTokenSequence().joinToString(",\n") { "${it.token}" })
+    val parser = Parser(lexer)
+    println("Ast: " + parser.generateAbstractSyntaxTree())
 }
 
