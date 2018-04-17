b10bd1e6d662b6488ca37ceffc162068931e0a34
Thomas Knudsen <teknight@live.dk>
2018-04-17 11:41:03 +0200

    Jira id: HCLANG-76 - #comment Fixed test

--- code/src/main/kotlin/parser/Parser.kt
+++ code/src/main/kotlin/parser/Parser.kt
@@ -257,8 +257,8 @@ class Parser(val lexer: ILexer): IParser, ITypeChecker by TypeChecker(),
                     Token.SpecialChar.BlockEnd -> scopeDepth -= 1
                 }
                 if (scopeDepth == 0) {
-                    if (hasAhead(ahead + 1)) {
-                        return (lookAhead(ahead + 1).token as? Token.Identifier)?.let {
+                    if (hasAhead(ahead)) {
+                        return (lookAhead(ahead).token as? Token.Identifier)?.let {
                             return it.value
                         }
                     } else return null
