a8aba31cef83c43689bad056b95b054d092f00b2
Unknown <tobiaslambek@gmail.com>
2018-04-25 15:26:26 +0200

    WIP: Added some built in functions. We are not sure about some things

--- code/src/main/kotlin/builtins/HclBuiltinFunctions.kt
+++ code/src/main/kotlin/builtins/HclBuiltinFunctions.kt
@@ -2,29 +2,124 @@ package builtins
 
 import parser.AstNode
 import parser.AstNode.Type
+import kotlin.reflect.full.createInstance
 
 private data class Parameter(val identifier: String, val type: Type)
 
 object HclBuiltinFunctions {
-    val functions = listOf(
-            buildOperator("+"),
-            buildOperator("plus", "+"),
-            buildOperator("-"),
-            buildOperator("*"),
-            buildOperator("/")
-    )
+    val functions =
+    // Operators
+            listOf(
+                    buildOperatorNumNumToNum("+"),
+                    buildOperatorNumNumToNum("plus", "+"),
+                    buildOperatorNumNumToNum("-"),
+                    buildOperatorNumNumToNum("*"),
+                    buildOperatorNumNumToNum("/"),
+
+                    buildOperatorNumNumToBool("<"),
+                    buildOperatorNumNumToBool(">"),
+
+                    buildOperatorTxtTxtToBool("=="),
+                    buildOperatorTxtTxtToBool("!="),
+
+                    buildOperatorBoolBoolToBool("&&"),
+                    buildOperatorBoolBoolToBool("||"),
+
+                    buildOperatorTxtTxtToTxt("+"),
+
+                    buildPrefixOperator<Type.Bool, Type.Bool>("negated", "!")
+            ) +
+            // Control structures
+            listOf(
+                    buildThenFunction(),
+                    buildWhileFunction(),
+                    buildGetLengthFunction()
+            )
 }
 
-private fun buildOperator(functionName: String, operator: String = functionName) = buildFunction(
+//region buildOperator_functions
+private fun buildOperatorNumNumToNum(functionName: String, operator: String = functionName) =
+        buildOperator<Type.Number, Type.Number, Type.Number>(functionName, operator)
+
+private fun buildOperatorNumNumToBool(functionName: String, operator: String = functionName) =
+        buildOperator<Type.Number, Type.Number, Type.Bool>(functionName, operator)
+
+private fun buildOperatorTxtTxtToBool(functionName: String, operator: String = functionName) =
+        buildOperator<Type.Text, Type.Text, Type.Bool>(functionName, operator)
+
+private fun buildOperatorTxtTxtToTxt(functionName: String, operator: String = functionName) =
+        buildOperator<Type.Text, Type.Text, Type.Text>(functionName, operator)
+
+private fun buildOperatorBoolBoolToBool(functionName: String, operator: String = functionName) =
+        buildOperator<Type.Bool, Type.Bool, Type.Bool>(functionName, operator)
+
+private inline fun<reified P, reified R> buildPrefixOperator(functionName: String, operator: String = functionName)
+        where P : Type, R : Type = buildFunction(
         identifier = functionName,
         parameters = listOf(
-                Parameter("leftHand", Type.Number),
-                Parameter("rightHand", Type.Number)
+                Parameter("operand", P::class.objectInstance!!)
         ),
-        returnType = Type.Number,
+        returnType = R::class.objectInstance!!,
+        body = "return $operator operand;",
+        inLine = true
+)
+
+private inline fun<reified V, reified H, reified R> buildOperator(functionName: String, operator: String = functionName)
+        where V : Type, H : Type, R : Type = buildFunction(
+        identifier = functionName,
+        parameters = listOf(
+                Parameter("leftHand", V::class.objectInstance!!),
+                Parameter("rightHand", H::class.objectInstance!!)
+        ),
+        returnType = R::class.objectInstance!!,
         body = "return leftHand $operator rightHand;",
         inLine = true
 )
+//endregion buildOperator_functions
+
+//region builtInFunctions
+private inline fun<reified P: Type> buildToStringFunction() = buildFunction(
+        identifier = "toString",
+        parameters = listOf(
+                Parameter("input", P::class.objectInstance!!)
+        ),
+        returnType = Type.Bool,
+        body = "return input;", //TODO Fix this to it actually returns a string!
+        inLine = false
+)
+
+private fun buildGetLengthFunction() = buildFunction(
+        identifier = "length",
+        parameters = listOf(
+                Parameter("list", Type.List(Type.Number)) //TODO Somehow make this generic
+        ),
+        returnType = Type.Number,
+        body = "return list.size();", //TODO Make sure the List class has a size() function
+        inLine = true
+)
+
+private fun buildWhileFunction() = buildFunction(
+        identifier = "while",
+        parameters = listOf(
+                Parameter("body", Type.Func.ExplicitFunc(listOf(), Type.None)),
+                Parameter("condition", Type.Bool)
+        ),
+        returnType = Type.Bool,
+        body = "while (condition) { body(); }",
+        inLine = true
+)
+
+private fun buildThenFunction() = buildFunction(
+        identifier = "then",
+        parameters = listOf(
+                Parameter("condition", Type.Bool),
+                Parameter("body", Type.Func.ExplicitFunc(listOf(), Type.None))
+        ),
+        returnType = Type.Bool,
+        body = "if (condition) { body(); } return condition;",
+        inLine = true
+)
+//endregion builtInFunctions
 
 private fun buildFunction(identifier: String, parameters: List<Parameter>, returnType: Type,
                           body: String, inLine: Boolean) =
