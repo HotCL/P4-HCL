2667f192107277d4559f81f91658c26e70499b76
Tobias Lambek Jacobsen <tobiaslambek@gmail.com>
2018-05-09 16:20:25 +0200

    Renamings and fix for readAnaPin

--- code/cppRaw/cppRawArduinoPinFunctions/arduinopin.h
+++ code/cppRaw/cppRawArduinoPinFunctions/arduinopin.h
@@ -2,30 +2,30 @@
 #define ARDUINOPIN_H
 
 #ifdef ARDUINO_AVR_UNO
-void setDigPinOut(double dPin, int value) {
+void writeDigPin(double dPin, int value) {
     int pin = (int)round(dPin);
     pinMode(pin, OUTPUT);
     digitalWrite(pin, value);
     return;
 }
 
-double setDigPinIn(double dPin) {
+double readDigPin(double dPin) {
     int pin = (int)round(dPin);
     pinMode(pin, INPUT);
     return (double)digitalRead(pin);
 }
 
-void setAnaPinOut(double aPin, int value) {
+void writeAnaPin(double aPin, int value) {
     int pin = (int)round(aPin);
     pinMode(pin, OUTPUT);
     analogWrite(pin, value);
     return;
 }
 
-double setAnaPinIn(double aPin) {
+double readAnaPin(double aPin) {
     int pin = (int)round(aPin);
     pinMode(pin, INPUT);
-    return analogWrite(pin);
+    return analogRead(pin);
 }
 #else
 #include <iostream>
@@ -33,25 +33,25 @@ double setAnaPinIn(double aPin) {
 /*
  * These functions are only for testing when program is run on PC
  */
-void setDigPinOut(double dPin, int value) {
+void writeDigPin(double dPin, int value) {
     int pin = (int)round(dPin);
     std::cout << "Set digital pin " << pin << " to output " << (value == 0 ? "LOW" : "HIGH") << std::endl;
     return;
 }
 
-double setDigPinIn(double dPin) {
+double readDigPin(double dPin) {
     int pin = (int)round(dPin);
     std::cout << "Reading from digital pin " << pin << std::endl;
     return 0.0; //arbitrary value
 }
 
-void setAnaPinOut(double aPin, int value) {
+void writeAnaPin(double aPin, int value) {
     int pin = (int)round(aPin);
     std::cout << "Set analog pin " << pin << " to output " << value << std::endl;
     return;
 }
 
-double setAnaPinIn(double aPin) {
+double readAnaPin(double aPin) {
     int pin = (int)round(aPin);
     std::cout << "Reading from analog pin " << pin << std::endl;
     return 0.0; //arbitrary value
