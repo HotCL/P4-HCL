d33af2d0cccf0420f1c9af6d0ad7b35e52c8c302
Nicolaj <nicolaj.abildgaard@hotmail.com>
2018-05-23 12:52:29 +0200

    Changed names of built-ins
    Also replaces all occurences of former names.

--- code/src/main/kotlin/builtins/HclBuiltinFunctions.kt
+++ code/src/main/kotlin/builtins/HclBuiltinFunctions.kt
@@ -253,7 +253,7 @@ private fun buildLengthText() =
 
 private fun buildSubtextText() =
     buildFunction(
-        identifier = "subText",
+        identifier = "splitAt",
         parameters = listOf(
             Parameter("leftHand", Type.Text),
             Parameter("startIndex", Type.Number),
@@ -264,7 +264,7 @@ private fun buildSubtextText() =
     )
 
 private fun buildSubListFunction() = buildFunction(
-    identifier = "subList",
+    identifier = "splitAt",
     parameters = listOf(
         Parameter("list", Type.List(Type.GenericType("T"))),
         Parameter("startIndex", Type.Number),
@@ -309,7 +309,7 @@ private fun buildThenFunction() = buildFunction(
 )
 
 private fun buildEachFunction() = buildFunction(
-    identifier = "each",
+    identifier = "forEach",
     parameters = listOf(
         Parameter("list", Type.List(Type.GenericType("T"))),
         Parameter("body", Type.Func(listOf(Type.GenericType("T")), Type.None))
@@ -339,7 +339,7 @@ private fun buildMapFunction() = buildFunction(
 )
 
 private fun buildFilterFunction() = buildFunction(
-    identifier = "filter",
+    identifier = "where",
     parameters = listOf(
         Parameter("list", Type.List(Type.GenericType("T"))),
         Parameter("fun",
@@ -360,7 +360,7 @@ private fun buildFilterFunction() = buildFunction(
 )
 
 private fun buildDelayMillisFunction() = buildFunction(
-    identifier = "delayMillis",
+    identifier = "delay",
     parameters = listOf(Parameter("millis", Type.Number)),
     returnType = Type.None,
     body = "#if defined(ARDUINO_AVR_UNO) || defined(ESP8266)\n" +
