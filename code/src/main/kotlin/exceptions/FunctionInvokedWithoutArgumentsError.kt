b4135709fcc83da1a74c3c00c219179e304d574c
Unknown <nillas12@gmail.com>
2018-05-26 10:25:57 +0200

    Fixed linting

--- code/src/main/kotlin/exceptions/FunctionInvokedWithoutArgumentsError.kt
+++ code/src/main/kotlin/exceptions/FunctionInvokedWithoutArgumentsError.kt
@@ -4,10 +4,12 @@ package exceptions
  * Class used to log when user tries to call an infix function without arguments.
  * @param nameOfFunc The name of the function that is being called
  */
-class FunctionInvokedWithoutArgumentsError(lineNumber: Int,
-                                           lineIndex: Int,
-                                           lineText: String,
-                                           private val nameOfFunc: String)
+class FunctionInvokedWithoutArgumentsError(
+    lineNumber: Int,
+    lineIndex: Int,
+    lineText: String,
+    private val nameOfFunc: String
+)
     : ParserException(lineNumber, lineIndex, lineText) {
     override val errorMessage = "Function $nameOfFunc cannot be called with 0 arguments!"
     override val helpText = "Make sure you are calling the correct function."
